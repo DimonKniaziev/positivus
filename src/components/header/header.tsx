ec35c9ce2016a5b190598b65fdcca661d2d314ac
DimonKniaziev <dimkniazz@gmail.com>
2025-01-21 21:55:33 +0200

    add-flexibility-for-header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,21 +2,26 @@ import React from "react";
 import './header.scss'
 import positivusLabel from './Positivus.svg';
 import Image from "next/image";
+import mobileMenuIcon from './mobile-menu-icon.svg'
 
 const Header: React.FC = () => {
     return (
         <header>
-            <div className="positivus-logo">
-                <svg className="logo-icon" width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M17.9986 5.53472L35.5997 0.349121L30.464 18.0001L35.5997 35.6012L17.9986 30.4655L0.347656 35.6012L5.53325 18.0001L0.347656 0.349121L17.9986 5.53472Z" fill="black"/>
-                </svg>
+            <Image
+                src={positivusLabel}
+                width={220}
+                height={36}
+                alt="positivus"
+                className="positivus-logo"
+            />
+            <button className="mobile-menu-button">
                 <Image
-                    src={positivusLabel}
-                    width={171}
-                    height={29}
-                    alt="positivus"
+                    src={mobileMenuIcon}
+                    width={24}
+                    height={16}
+                    alt="mobile menu"
                 />
-            </div>
+            </button>
             <div className="nav-bar">
                 <ul className="links-list">
                     <li>About us</li>
