f9eab8e05793c37ad7b8e96c4299211b24e8fbd9
DimonKniaziev <dimkniazz@gmail.com>
2025-01-21 22:50:26 +0200

    adding-flexibility-for-main-1-component

--- src/components/main-1/main-1.tsx
+++ src/components/main-1/main-1.tsx
@@ -17,59 +17,77 @@ const Main1: React.FC = () => {
                     <h1>
                         Navigating the digital landscape for success
                     </h1>
-                    <span>
-                        Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.
-                    </span>
-                    <button>
-                        Book a consultation
-                    </button>
-                </div>
-                <div className="main-1-image">
                     <Image
                         src={illustration}
                         width={600}
                         height={515}
                         alt="illustration-1"
+                        className="main-1-mobile-image"
                     />
-                </div>            
+                    <p>
+                        Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.
+                    </p>
+                    <button>
+                        Book a consultation
+                    </button>
+                </div>
+                <Image
+                    src={illustration}
+                    width={600}
+                    height={515}
+                    alt="illustration-1"
+                    className="main-1-image"
+                />
             </div>
             <div className="main-1-companies">
-                <Image
-                    src={amazonLogo}
-                    width={124}
-                    height={48}
-                    alt="amazon"
-                />
-                <Image
-                    src={dribbleLogo}
-                    width={126}
-                    height={48}
-                    alt="dribble"
-                />
-                <Image
-                    src={hubspotLogo}
-                    width={129}
-                    height={48}
-                    alt="hubspot"
-                />
-                <Image
-                    src={notionLogo}
-                    width={146}
-                    height={48}
-                    alt="notion"
-                />
-                <Image
-                    src={netflixLogo}
-                    width={125}
-                    height={48}
-                    alt="netflix"
-                />
-                <Image
-                    src={zoomLogo}
-                    width={111}
-                    height={48}
-                    alt="zoom"
-                />
+                <div className="img-container">
+                    <Image
+                        src={amazonLogo}
+                        width={124}
+                        height={48}
+                        alt="amazon"
+                    />
+                </div>
+                <div className="img-container">
+                    <Image
+                        src={dribbleLogo}
+                        width={126}
+                        height={48}
+                        alt="dribble"
+                    />
+                </div>
+                <div className="img-container">
+                    <Image
+                        src={hubspotLogo}
+                        width={129}
+                        height={48}
+                        alt="hubspot"
+                    />
+                </div>
+                <div className="img-container">
+                    <Image
+                        src={notionLogo}
+                        width={146}
+                        height={48}
+                        alt="notion"
+                    />
+                </div>
+                <div className="img-container">
+                    <Image
+                        src={netflixLogo}
+                        width={125}
+                        height={48}
+                        alt="netflix"
+                    />
+                </div>
+                <div className="img-container">
+                    <Image
+                        src={zoomLogo}
+                        width={111}
+                        height={48}
+                        alt="zoom"
+                    />
+                </div>
             </div>
         </React.Fragment>
     )
