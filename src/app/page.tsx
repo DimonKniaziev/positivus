e4f6db2a2548f46a363f3a1050c3530d874f67f6
DimonKniaziev <dimkniazz@gmail.com>
2025-01-21 20:57:30 +0200

    add-all-components-to-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
+import Header from "@/components/header";
+import Main1 from "@/components/main-1";
+import Services from "@/components/services";
+import GetProposal from "@/components/get-proposal";
+import CaseStudies from "@/components/case-studies";
+import WorkingProcess from "@/components/working-process";
+import Team from "@/components/team";
+import Testimonials from "@/components/testimonials";
+import ContactUs from "@/components/contact-us";
+import Footer from "@/components/footer";
+
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <Header/>
+      <Main1/>
+      <Services/>
+      <GetProposal/>
+      <CaseStudies/>
+      <WorkingProcess/>
+      <Team/>
+      <Testimonials/>
+      <ContactUs/>
+      <Footer/>
+    </div>
   );
 }
